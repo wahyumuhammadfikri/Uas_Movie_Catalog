874450803353044037ceb12acab8f4a282621e2b
Yusuf <1910031802142@sar.ac.id>
2022-07-07 20:04:43 +0700

    commit first

---
+++ app/src/main/java/com/example/wahyumuhammafikri/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.wahyumuhammafikri
+
+class Constant{
+    companion object {
+        const val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
+    }
+}
---
+++ app/src/main/java/com/example/wahyumuhammafikri/MovieViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.wahyumuhammafikri
+
+import androidx.lifecycle.ViewModel
+
+class MovieViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
